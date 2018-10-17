b132f057d5796160ff5ebc034eb0d23fb7e63073
Henrik Hedlund Andersen <heheand@gmail.com>
2018-10-17 22:11:12 +0200

    WalkDir implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+
+    match traverse() {
+        Ok(()) => std::process::exit(0),
+        Err(err) => {
+            eprintln!("{}", err);
+
+            std::thread::sleep(std::time::Duration::from_millis(10000));
+            std::process::exit(2);
+        }
+    }
 }
+
+
+pub fn traverse() -> Result<(),std::io::Error> {
+    let directory: &'static str = "C:\\";
+
+    let mut file = File::create("c.txt")?;
+
+    for entry in WalkDir::new(directory).into_iter().filter_map(|e| e.ok()) {
+        //println!("path: \"{}\", size: {}", entry.path().display(), fs::metadata(entry.path()).unwrap().len());
+        let line = format!("path: \"{}\", size: {}\n", entry.path().display(), fs::metadata(entry.path())?.len());
+        file.write_all(line.as_bytes())?;
+    }
+    Ok(())
+} 
+
+/*pub fn traverse_folder<P>(path: P) -> Result<Vec<PathBuf>, std::io::Error>
+    where P: AsRef<Path> {
+        fs::read_dir(path)?
+        .into_iter()
+        .map(|x| x.map(|entry| entry.path()))
+        .collect()
+    }
+
+    */
